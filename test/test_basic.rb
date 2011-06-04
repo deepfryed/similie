cc5f9298b0109f769f9db145a5c04cb3e1384ffb
Bharanee Rathna <deepfryed@gmail.com>
2011-06-04 14:55:56 +1000

    tests: jpg => png

--- test/test_basic.rb
+++ test/test_basic.rb
@@ -2,15 +2,15 @@ require_relative 'helper'
 
 describe 'Similie image load' do
   it 'should load image' do
-    assert Similie.new(File.join($testdir, 'lena1.jpg'))
+    assert Similie.new(File.join($testdir, 'lena1.png'))
   end
 
   it 'should barf on invalid path' do
-    assert_raises(ArgumentError) { Similie.new(File.join($testdir, 'lena2.jpg')) }
+    assert_raises(ArgumentError) { Similie.new(File.join($testdir, 'foo')) }
   end
 
   it 'should hash image' do
-    img = Similie.new(File.join($testdir, 'lena1.jpg'))
+    img = Similie.new(File.join($testdir, 'lena1.png'))
     assert img
     assert_equal 216455360913932544, img.hash
   end
--- test/test_distance.rb
+++ test/test_distance.rb
@@ -2,7 +2,7 @@ require_relative 'helper'
 
 describe 'Similie image distance' do
   it 'should work for similar images' do
-    images = %w(lena1.jpg lena2.png lena3.png lena4.png lena5.jpg).map {|file| Similie.new(File.join($testdir, file))}
+    images = (1..5).map {|n| Similie.new(File.join($testdir, 'lena%d.png' % n ))}
 
     assert_equal 1, images[0].distance(images[1])
     assert_equal 2, images[1].distance(images[2])
@@ -11,6 +11,6 @@ describe 'Similie image distance' do
   end
 
   it 'should work using the singleton method' do
-    assert_equal 6, Similie.distance(File.join($testdir, 'lena4.png'), File.join($testdir, 'lena5.jpg'))
+    assert_equal 6, Similie.distance(File.join($testdir, 'lena4.png'), File.join($testdir, 'lena5.png'))
   end
 end
