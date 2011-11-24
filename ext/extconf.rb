14318bb9b50e50b03f6c64a0a1da8ebe939f487c
Bharanee Rathna <deepfryed@gmail.com>
2011-11-24 20:03:44 +1100

    macos build hack

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -10,22 +10,24 @@ def lib_paths lib, defaults
   path.size > 0 ? path : defaults.map {|name| "-L#{name}"}.join(' ')
 end
 
-def lib_names lib, defaults
+def lib_flags lib, defaults
   path = %x{pkg-config #{lib}  --libs-only-l 2>/dev/null}.strip
   path.size > 0 ? path : defaults.map {|name| "-l#{name}"}.join(' ')
 end
 
-$CFLAGS  = inc_paths('opencv', %w(/usr/include/opencv)) + ' -Wall'
-$LDFLAGS = lib_names('opencv', %w(highgui cxcore))
-cxcore   = $LDFLAGS.scan(%r{-l(\w*core)}).flatten.first
-highgui  = $LDFLAGS.scan(%r{-l(\w*highgui)}).flatten.first
+def lib_name re
+  $LDFLAGS.scan(re).flatten.first
+end
 
-raise "unable to find opencv cxcore"  unless cxcore
-raise "unable to find opencv highgui" unless highgui
+$CFLAGS  = inc_paths 'opencv', %w(/usr/include/opencv)
+$LDFLAGS = lib_flags 'opencv', %w(highgui cxcore)
 
-headers = %w(stdio.h stdlib.h string.h opencv/cxcore.h opencv/highgui.h)
-lib_1   = [cxcore,  'cvInitFont',    headers]
-lib_2   = [highgui, 'cvEncodeImage', headers]
+cxcore   = lib_name(%r{-l(\w*core)})    or raise 'unable to find opencv cxcore'
+highgui  = lib_name(%r{-l(\w*highgui)}) or raise 'unable to find opencv highgui'
+
+headers  = %w(stdio.h stdlib.h string.h opencv/cxcore.h opencv/highgui.h)
+lib_1    = [cxcore,  'cvInitFont',    headers]
+lib_2    = [highgui, 'cvEncodeImage', headers]
 
 if have_header('opencv/cxcore.h') && have_library(*lib_1) && have_library(*lib_2)
   create_makefile 'similie'
@@ -35,6 +37,9 @@ else
 
     On debian based systems you can install it from apt as,
       sudo apt-get install libcv-dev libhighgui-dev
+
+    On macos try,
+      brew install opencv
 
     Refer to http://opencv.willowgarage.com/wiki/InstallGuide for other platforms or operating systems.
   }
