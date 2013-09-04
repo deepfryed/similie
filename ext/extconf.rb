1a16a835e31df1de00d6ce4e6d7c7dec6fba06b6
Ivan Kuchin <tadump+git@gmail.com>
2013-09-04 13:15:19 +0200

    exconf minor cleanup

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,26 +2,29 @@ require 'mkmf'
 
 def inc_paths lib, defaults
   path = %x{pkg-config #{lib} --cflags 2>/dev/null}.strip
-  path.size > 0 ? path : defaults.map {|name| "-I#{name}"}.join(' ')
+  path.size > 0 ? path : defaults.map{ |name| "-I#{name}"}.join(' ')
 end
 
 def lib_paths lib, defaults
   path = %x{pkg-config #{lib}  --libs-only-L 2>/dev/null}.strip
-  path.size > 0 ? path : defaults.map {|name| "-L#{name}"}.join(' ')
+  path.size > 0 ? path : defaults.map{ |name| "-L#{name}"}.join(' ')
 end
 
 def lib_flags lib, defaults
   path = %x{pkg-config #{lib}  --libs-only-l 2>/dev/null}.strip
-  path.size > 0 ? path.split(/\s+/).select {|name| %r{core|highgui}.match(name)}.join(' ')
-                : defaults.map {|name| "-l#{name}"}.join(' ')
+  if path.size > 0
+    path.split(/\s+/).select{ |name| %r{core|highgui}.match(name)}.join(' ')
+  else
+    defaults.map{ |name| "-l#{name}"}.join(' ')
+  end
 end
 
 def lib_name re
   $LDFLAGS.scan(re).flatten.first
 end
 
-$CFLAGS  = inc_paths 'opencv', %w(/usr/include/opencv)
-$LDFLAGS = lib_flags 'opencv', %w(opencv_highgui opencv_core)
+$CFLAGS  = inc_paths 'opencv', %w[/usr/include/opencv]
+$LDFLAGS = lib_flags 'opencv', %w[opencv_highgui opencv_core]
 
 cxcore   = lib_name(%r{-l(\w*core)})    or raise 'unable to find opencv cxcore'
 highgui  = lib_name(%r{-l(\w*highgui)}) or raise 'unable to find opencv highgui'
@@ -33,7 +36,7 @@ lib_2    = [highgui, 'cvEncodeImage', headers]
 if have_header('opencv/cxcore.h') && have_library(*lib_1) && have_library(*lib_2)
   create_makefile 'similie'
 else
-  puts %q{
+  abort %q{
     Cannot find opencv headers or libraries.
 
     On debian based systems you can install it from apt as,
@@ -44,6 +47,4 @@ else
 
     Refer to http://opencv.willowgarage.com/wiki/InstallGuide for other platforms or operating systems.
   }
-
-  exit 1
 end
